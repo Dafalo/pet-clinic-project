db18cd873b60624c9bc9a3a72319ef198abcac77
Ekaterina Menshenina <Ekaterina_Menshenina@epam.com>
2020-09-02 16:25:18 +0300

    added Vets List and update VetController

--- pet-clinic-web/src/main/java/spring/project/petclinicproject/controllers/VetController.java
+++ pet-clinic-web/src/main/java/spring/project/petclinicproject/controllers/VetController.java
@@ -1,13 +1,22 @@
 package spring.project.petclinicproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.project.petclinicproject.services.VetService;
 
 @Controller
 public class VetController {
 
-  @RequestMapping("/vets")
-  public String listVets() {
-    return "vets/index";
-  }
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping("/vets")
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
+    }
 }
